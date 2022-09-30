5192916f341b4607b975d63c74fa37838f9008e4
Anooshik Vartanian <vartanian.anooshik@gmail.com>
2022-09-30 18:20:40 +0400

    first commit. added components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { BoardComponent } from './board/board.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    BoardComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
